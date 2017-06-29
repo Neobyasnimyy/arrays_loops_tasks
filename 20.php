5945e1640334afe1bc80b08cf53ea85ac3145954
Borysiuk Andrey <factodessa@gmail.com>
2017-06-29 21:15:08 +0300

    number 20 is True

---
+++ 20.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <style>
+        body{
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        for ($i=1;$i<=20;$i++){
+            echo str_repeat('x',$i),"<br>";
+        }
+    ?>
+
+
+</body>
+</html>
