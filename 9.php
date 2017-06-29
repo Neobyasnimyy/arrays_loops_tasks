00173b193318e2757be08673b7e5ed959d4e56b7
Borysiuk Andrey <factodessa@gmail.com>
2017-06-29 18:33:24 +0300

    number 9 is True

---
+++ 9.php
@@ -0,0 +1,17 @@
+<?php
+$i=0;
+while ($i++ < 100){
+    print "$i";
+    print "<br>";
+}
+
+$i=1;
+while ($i <= 100){
+    print "$i";
+    print "<br>";
+    $i++;
+}
+for ($k=1;$k<=100;$k++){
+    print "k = $k";
+    print "<br>";
+}
