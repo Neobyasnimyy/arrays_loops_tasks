2afd99940d9f9f15aa74848b1d62cbc43d1f4042
Borysiuk Andrey <factodessa@gmail.com>
2017-06-29 18:46:48 +0300

    number 10 is True

---
+++ 10.php
@@ -0,0 +1,20 @@
+<?php
+$i=11;
+while ($i <= 33){
+    print "$i";
+    print "<br>";
+    $i++;
+}
+
+for ($k=11;$k<=33;$k++){
+    print "k = $k";
+    print "<br>";
+}
+
+$i=0;
+while ($i++ < 100){
+    if ($i>=11 and $i <=33){
+        print "i = $i";
+        print "<br>";
+    }
+}
