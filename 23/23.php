9c7ed764ca95fb36ef8805f2d8c778c35733a231
Borysiuk Andrey <factodessa@gmail.com>
2017-06-29 23:01:04 +0300

    number 23 is True

--- 23/23.php
+++ 23/23.php
@@ -1,11 +1,24 @@
 <?php
 
-include 'post.html';
+include 'get.html';
 $number = $_GET['number'];
 $result = 0;
 for ($i=0;$i<strlen($number);$i++){
     $result+= (int)$number[$i];
 }
 echo "<br>";
-echo "Сумма цыфр равна $result";
+echo "Сумма цыфр 1 строки равна $result";
+
+$number2 = $_GET['number2'];
+$result2 = 0;
+if (is_numeric($number2)){
+    for ($i=0;$i<strlen($number2);$i++){
+        $result2+= (int)$number2[$i];
+    }
+    echo "<br>";
+    echo "Сумма цыфр 2 строки равна $result2";
+}else{
+    echo "<br>";
+    echo "Необходимо ввести числа во 2 строке";
+}
 
