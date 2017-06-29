efaa8b71a883f02e6abed846e700b4aefa8d5af5
Borysiuk Andrey <factodessa@gmail.com>
2017-06-29 17:18:34 +0300

    number 2 is True

---
+++ 2.php
@@ -0,0 +1,12 @@
+<?php
+
+$arr = [1, 20, 15, 17, 24, 35];
+$result = 0;
+foreach ($arr as $item) {
+    $result += $item;
+}
+echo "cумма элементов массыва [1, 20, 15, 17, 24, 35] равна $result";
+echo "<br>";
+// 2 вариант
+$result2 = array_sum($arr);
+echo "cумма элементов массыва [1, 20, 15, 17, 24, 35] равна $result";
