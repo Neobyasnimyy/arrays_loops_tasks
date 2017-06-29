720be4563258a03dec3cf0d220f881c9d5bfd0dc
Borysiuk Andrey <factodessa@gmail.com>
2017-06-29 20:34:59 +0300

    number 19 is True

---
+++ 19.php
@@ -0,0 +1,12 @@
+<?php
+$arr = ['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье'];
+$day = date("N");
+foreach ($arr as $key=>$item){
+    if ($key==$day-1){
+        echo "<b>$item</b>";
+    }else{
+        echo $item;
+    }
+    echo "<br>";
+
+}
